3f41bfc8cca6cebcc4fac1f6a0970787f7699bbd
as-troska <troska@asvg.no>
2026-05-06 09:29:15 +0200

    Starta Modul 3

---
+++ Modul 3/Oppgåve/main.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = ">= 4.38.1"
+        }
+    }
+  }
+
+provider "azurerm" {
+    subscription_id = var.subscription_id
+    features {
+
+    }
+}
---
+++ Modul 3/variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_id" {
+  description = "The Azure subscription ID."
+  type        = string
+  default     = "7a3c6854-0fe1-42eb-b5b9-800af1e53d70"
+}
+
+variable "base_name" {
+  description = "The base name for resources and resource groups."
+  type        = string
+  default     = "modul3"
+}
